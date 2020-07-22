4dba6efbe5aa385c960711b5bfa44c759f8121df
Gustaf Rosenblad <gustaf@ccode.se>
2020-07-22 17:09:18 +0200

    chore: Removed dependency on Vapor

--- Package.swift
+++ Package.swift
@@ -13,14 +13,12 @@ let package = Package(
         .package(name: "swift-log", url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(name: "grpc-swift", url: "https://github.com/grpc/grpc-swift.git", from: "1.0.0-alpha.12"),
         .package(name: "Auth", url: "https://github.com/googleapis/google-auth-library-swift.git", from: "0.5.2"),
-        .package(name: "vapor", url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
     ],
     targets: [
         .target(name: "GoogleCloudLogging", dependencies: [
             .product(name: "Logging", package: "swift-log"),
             .product(name: "GRPC", package: "grpc-swift"),
             .product(name: "OAuth2", package: "Auth"),
-            .product(name: "Vapor", package: "vapor"),
         ]),
         .testTarget( name: "GoogleCloudLoggingTests", dependencies: ["GoogleCloudLogging"]),
     ]
