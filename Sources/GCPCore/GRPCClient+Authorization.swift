8cf8cea7581a85e5760183d4fba7079ded5a0a16
Gustaf Rosenblad <gustaf@ccode.se>
2022-04-27 08:15:45 +0200

    fix: Skip authorization against GCP APIs in debug

--- Sources/GCPCore/GRPCClient+Authorization.swift
+++ Sources/GCPCore/GRPCClient+Authorization.swift
@@ -6,6 +6,15 @@ extension GRPCClient {
     /// Ensures the GRPC client has a valid authorization header for communication with GCP APIs.
     /// - Parameter client: The client to set authorization header on.
     public mutating func ensureAuthentication(authorization: inout Authorization) async throws {
+        #if DEBUG
+
+        // Don't authorize in debug, unless we have credentials
+        if ProcessInfo.processInfo.environment["GOOGLE_APPLICATION_CREDENTIALS"]?.isEmpty != false {
+            return
+        }
+
+        #endif
+
         let accessToken = try await authorization.token()
 
         defaultCallOptions.customMetadata.replaceOrAdd(
