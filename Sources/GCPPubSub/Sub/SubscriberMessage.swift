41011009f7c6ad36683c998773e1bc13f3386d41
Gustaf Rosenblad <gustaf@ccode.se>
2022-04-28 08:11:33 +0200

    feat(pubsub): Added initializer for subscriber message usable for testing

--- Sources/GCPPubSub/Sub/SubscriberMessage.swift
+++ Sources/GCPPubSub/Sub/SubscriberMessage.swift
@@ -20,6 +20,32 @@ public struct SubscriberMessage: Message {
         self.logger = logger
     }
 
+#if DEBUG
+    /// Only for testing. Use this initializer to create a message for testing a handler.
+    /// - Parameters:
+    ///   - id: Id for the message. Default always "0".
+    ///   - published: Date of when message was published. Default to current date. Change this if you want to test handlers with old messages.
+    ///   - data: Body data of the message.
+    ///   - attributes: Attributes for the message. Default empty dictionary.
+    public init(
+        id: String = "0",
+        published: Date = Date(),
+        data: Data,
+        attributes: [String: String] = [:]
+    ) {
+        var logger = Logger(label: "pubsub.subscriber.message")
+        logger[metadataKey: "message"] = .string("0")
+
+        self.init(
+            id: id,
+            published: published,
+            data: data,
+            attributes: attributes,
+            logger: logger
+        )
+    }
+#endif
+    
     public func decode<Element: SwiftProtobuf.Message>(_ elementType: Element.Type) throws -> Element {
         try elementType.init(serializedData: data)
     }
