d585380096cf7f768165064238a0eb91891aa1a7
Gustaf Rosenblad <gustaf@ccode.se>
2022-06-29 13:26:24 +0200

    feat(trace): Don’t append trace to logging in debug

--- Sources/GCPTrace/Extensions/Logger+Metadata.swift
+++ Sources/GCPTrace/Extensions/Logger+Metadata.swift
@@ -5,9 +5,11 @@ import GCPCore
 extension Logger {
 
     public mutating func addMetadata(for trace: Trace) {
+#if !DEBUG
         self[metadataKey: LogMetadataKeys.trace] = .string("projects/\(Tracing.projectID)/traces/\(trace.id.stringValue)")
         if let spanID = trace.spanID ?? trace.rootSpan?.id {
             self[metadataKey: LogMetadataKeys.spanID] = .string(spanID.stringValue)
         }
+#endif
     }
 }
