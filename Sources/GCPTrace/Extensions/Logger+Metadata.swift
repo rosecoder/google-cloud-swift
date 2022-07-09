ba6e94ec0a6cce6bf2d6260138246074f657483f
Gustaf Rosenblad <gustaf@ccode.se>
2022-07-09 15:45:08 +0200

    feat(trace): Added convenience Logger.init(label:,trace:)

--- Sources/GCPTrace/Extensions/Logger+Metadata.swift
+++ Sources/GCPTrace/Extensions/Logger+Metadata.swift
@@ -4,6 +4,14 @@ import GCPCore
 
 extension Logger {
 
+    public init(label: String, trace: Trace?) {
+        self.init(label: label)
+
+        if let trace = trace {
+            addMetadata(for: trace)
+        }
+    }
+
     public mutating func addMetadata(for trace: Trace) {
 #if !DEBUG
         self[metadataKey: LogMetadataKeys.trace] = .string("projects/\(Tracing.projectID)/traces/\(trace.id.stringValue)")
