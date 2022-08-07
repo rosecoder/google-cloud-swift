e966e6078ebe5a94821152e0a4e6b490d4bafb3e
Gustaf Rosenblad <gustaf@ccode.se>
2022-08-07 22:03:28 +0200

    feat(app): Simplified HTTPDependency API

--- Sources/GCPApp/HTTPDependency.swift
+++ Sources/GCPApp/HTTPDependency.swift
@@ -8,15 +8,26 @@ public protocol HTTPDependency: Dependency {
     static var host: String { get }
     static var port: Int { get }
 
-    static var _client: HTTPClient? { get }
-    static func initClient(eventLoopGroup: EventLoopGroup)
+    static var configuration: HTTPClient.Configuration { get }
+
+    static var _client: HTTPClient? { get set }
 }
 
 extension HTTPDependency {
 
+    // MARK: - Default Implementation
+
     public static var port: Int {
         isSecure ? 443 : 80
     }
+
+    public static var configuration: HTTPClient.Configuration {
+        .init(
+            timeout: .init(connect: .seconds(5), read: .seconds(60))
+        )
+    }
+
+    // MARK: - Client
 
     public static var client: HTTPClient {
         guard let _client = _client else {
@@ -25,23 +36,13 @@ extension HTTPDependency {
         return _client
     }
 
+    // MARK: - Bootstrap
+
     public static func bootstrap(eventLoopGroup: EventLoopGroup) async throws {
-#if DEBUG
-        try await bootstrapForDevelopment(eventLoopGroup: eventLoopGroup)
-#else
-        try await bootstrapForProduction(eventLoopGroup: eventLoopGroup)
-#endif
+        _client = HTTPClient(eventLoopGroupProvider: .shared(eventLoopGroup), configuration: configuration)
     }
 
-    private static func bootstrapForProduction(eventLoopGroup: EventLoopGroup) async throws {
-        self.initClient(eventLoopGroup: eventLoopGroup)
-    }
-
-#if DEBUG
-    private static func bootstrapForDevelopment(eventLoopGroup: EventLoopGroup) async throws {
-        self.initClient(eventLoopGroup: eventLoopGroup)
-    }
-#endif
+    // MARK: - Termination
 
     public static func shutdown() async throws {
         try await client.shutdown()
