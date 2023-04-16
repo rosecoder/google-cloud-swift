2680b33c6418fb17904f9d4b88b39e4502ab0287
Gustaf Rosenblad <gustaf@ccode.se>
2023-04-16 13:51:43 +0200

    fix: Naming of 62093a49

--- Sources/GCPApp/gRPC/GRPC.ClientInterceptor.swift
+++ Sources/GCPApp/gRPC/GRPC.ClientInterceptor.swift
@@ -2,7 +2,7 @@ import Foundation
 import GRPC
 import GCPTrace
 
-public func defaultClientTraceInterceptors<Request, Response, DependencyType: GRPCDependency>(_ dependencyType: DependencyType.Type) -> [ClientInterceptor<Request, Response>] {
+public func defaultClientInterceptors<Request, Response, DependencyType: GRPCDependency>(_ dependencyType: DependencyType.Type) -> [ClientInterceptor<Request, Response>] {
     [
         ClientTraceInterceptor(),
         ClientGoogleAuthInterceptor(dependencyType),
