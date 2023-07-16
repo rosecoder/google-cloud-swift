faea10d4fd8490f55aa2c9e9ca057d90b90d8faa
Gustaf Rosenblad <gustaf@ccode.se>
2023-07-17 00:30:15 +0200

    fix: 5d62e936f92c0ceaa63438e9877d6610477eadc5

--- Sources/PubSub/Sub/Push/PushSubscriber.HTTPHandler.swift
+++ Sources/PubSub/Sub/Push/PushSubscriber.HTTPHandler.swift
@@ -18,7 +18,10 @@ extension PushSubscriber {
             decoder.dataDecodingStrategy = .base64
 
             let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+            dateFormatter.calendar = Calendar(identifier: .iso8601)
+            dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+            dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
             decoder.dateDecodingStrategy = .formatted(dateFormatter)
 
             return decoder
