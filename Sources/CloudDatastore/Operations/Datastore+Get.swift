591e8cbe9a6cef4e8817dc410ecf66bc26726188
Gustaf Rosenblad <gustaf@ccode.se>
2024-06-08 10:11:07 +0200

    fix(datastore): getEntities may return incorrect key
    In case of multiple entites in the lookup have the same last child id, but different parents.

--- Sources/CloudDatastore/Operations/Datastore+Get.swift
+++ Sources/CloudDatastore/Operations/Datastore+Get.swift
@@ -20,21 +20,33 @@ extension Datastore {
         Entity: _Entity,
         Entity.Key: AnyKey
     {
+        let rawKeys = keys.map { $0.raw }
+
         let response: Google_Datastore_V1_LookupResponse = try await context.trace.recordSpan(named: "datastore-lookup", kind: .client, attributes: [
             "datastore/kind": Entity.Key.kind,
         ]) { span in
             try await withRetryableTask(logger: context.logger, operation: {
                 try await shared.client(context: context).lookup(.with {
                     $0.projectID = projectID
-                    $0.keys = keys.map({ $0.raw })
+                    $0.keys = rawKeys
                 })
             }, file: file, function: function, line: line)
         }
 
         let decoder = EntityDecoder()
 
-        return try keys.map { key in
-            if let raw = response.found.first(where: { $0.entity.key.path.last!.idType == key.id.raw }) {
+        return try rawKeys.map { rawKey in
+            if let raw = response.found.first(where: {
+                guard rawKey.path.count == $0.entity.key.path.count else {
+                    return false
+                }
+                for (lhsPath, rhsPath) in zip(rawKey.path, $0.entity.key.path) {
+                    if lhsPath.kind != rhsPath.kind || lhsPath.idType != rhsPath.idType {
+                        return false
+                    }
+                }
+                return true
+            }) {
                 return try decoder.decode(Entity.self, from: raw.entity)
             } else {
                 return nil
