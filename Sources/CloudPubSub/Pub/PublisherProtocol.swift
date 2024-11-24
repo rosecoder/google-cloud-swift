69d0abac6e8d21aedecb54a601cc98b53cf6ec73
Gustaf Rosenblad <gustaf@ccode.se>
2024-11-24 13:18:57 +0100

    fix(pubsub): Default implementation of publisher with multiple messages

--- Sources/CloudPubSub/Pub/PublisherProtocol.swift
+++ Sources/CloudPubSub/Pub/PublisherProtocol.swift
@@ -15,12 +15,12 @@ extension PublisherProtocol {
     @discardableResult
     public func publish<Message>(
         to topic: Topic<Message>,
-        message: Message.Outgoing,
+        messages: [Message.Outgoing],
         file: String = #fileID,
         function: String = #function,
         line: UInt = #line
     ) async throws -> [PublishedMessage] {
-        try await publish(to: topic, messages: [message], file: file, function: function, line: line)
+        try await publish(to: topic, messages: messages, file: file, function: function, line: line)
     }
 
     @discardableResult
